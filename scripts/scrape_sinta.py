e58bd8e27a1ab6e34dcf2655c28c9d7a09e29e41
Rifky Fauzi <rifkyfauzi9@gmail.com>
2026-09-25 06:05:37 +0700

    Update scrape_sinta.py

--- scripts/scrape_sinta.py
+++ scripts/scrape_sinta.py
@@ -125,17 +125,11 @@ class SintaSession:
         try:
             response = self.http.get(
                 ZENROWS_ENDPOINT,
-                # params={
-                #     "apikey": self.api_key,
-                #     "url": target_url,
-                #     "premium_proxy": "true",
-                #     "proxy_country": "id",
-                #     "session_id": self.session_id,
-                # }
                 params={
                     "apikey": self.api_key,
                     "url": target_url,
-                    "mode": "auto",
+                    "premium_proxy": "true",
+                    "proxy_country": "id",
                     "session_id": self.session_id,
                 },
                 timeout=self.timeout,
